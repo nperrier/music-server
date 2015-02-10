bd1dc13182ba35c36c0324b06eb719cfb6cc8a7d
Nathan Perrier <nathan.perrier@code42.com>
2015-02-09 23:18:37 -0600

    Fix bug with artist name query (should search case insensitive)

--- src/main/java/com/perrier/music/entity/artist/ArtistFindByNameQuery.java
+++ src/main/java/com/perrier/music/entity/artist/ArtistFindByNameQuery.java
@@ -9,17 +9,16 @@ import com.perrier.music.db.FindQuery;
 public class ArtistFindByNameQuery extends FindQuery<Artist> {
 
 	private final String name;
-	
+
 	public ArtistFindByNameQuery(String name) {
 		this.name = name;
 	}
-	
+
 	@Override
 	public Artist query(Session session) throws DBException {
-		
-		Query q = session.createQuery("from Artist where name = :name");
+		Query q = session.createQuery("from Artist where name = lower(:name)");
 		q.setString("name", name);
-		
+
 		return (Artist) q.uniqueResult();
 	}
 
