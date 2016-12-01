c60161c853a861f74aa578319c687e7baa4833ef
Nathan Perrier <nathan.perrier@gmail.com>
2016-12-01 00:09:05 -0600

    Remove H2 full text init function call

--- server/src/main/resources/db/migration/V0__Init.sql
+++
@@ -1,4 +0,0 @@
-/* Enable full-text search */
-
-create alias if not exists FT_INIT for "org.h2.fulltext.FullText.init";
-call FT_INIT();
--- server/src/main/resources/db/migration/V1__Create_artist_table.sql
+++ server/src/main/resources/db/migration/V1__Create_artist_table.sql
@@ -1,10 +1,10 @@
 /* Create artist table */
 
 create table artist (
-    id serial not null,
-    name varchar(255) not null,
-    creation_date timestamp not null,
-    modification_date timestamp not null,
-    primary key(id),
-    unique(name)
-  );
+  id serial not null,
+  name varchar(255) not null,
+  creation_date timestamp not null,
+  modification_date timestamp not null,
+  primary key(id),
+  unique(name)
+);
