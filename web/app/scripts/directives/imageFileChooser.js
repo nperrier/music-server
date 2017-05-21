f8db2b1a338ae6509d7ed466cbc2fda7dfb5da4b
Nathan Perrier <nathan.perrier@gmail.com>
2017-05-20 22:14:53 -0500

    Allow adding cover art to tracks that initially have no cover artwork
    Needed to add a dummy image that would be visible if track's cover art was null

--- web/app/scripts/directives/imageFileChooser.js
+++ web/app/scripts/directives/imageFileChooser.js
@@ -2,17 +2,13 @@
 
 angular.module('musicApp').directive('imageFileChooser', [
   function () {
-
     return {
       restrict: 'E',
       scope: {
         'imageModel': '=',
         'imageSrc': '='
       },
-      template: '' +
-          '<img ng-src="{{imageSrc}}" class="track-edit-image" ng-click="fileChooser()"/>' +
-          '<input class="input-edit-image" type="file" style="display: none;" ' +
-                 'file-input="imageModel.file" file-preview="imageSrc" />',
+      templateUrl: '/views/imageFileChooser.html',
       link: function(scope, element) {
         scope.fileChooser = function fileChooser() {
           var fileInput = element[0].querySelector('.input-edit-image');
